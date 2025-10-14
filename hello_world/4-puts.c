7776d6e63a3ed4f6b7c9b516763f4d3df6597a23
Hamsa1913 <Hamsa12021@gmail.com>
2025-10-14 09:21:51 +0000

    add

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Description: Prints a quote about programming.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return (0);
 }
