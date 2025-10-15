cd405064749295abe47a499dbfd7445d17769b5c
Hams1913 <Hamsa1913@gmail.com>
2025-10-15 15:38:18 +0000

    update

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    char c;
+	char letter = 'a';
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-    
-    return 0;
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
 }
