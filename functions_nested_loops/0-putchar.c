b0d7e5de7dd533ff7be152ad386ce0fac971517f
Hams1913 <Hamsa1913@gmail.com>
2025-10-18 08:07:04 +0000

    update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,10 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints _putchar followed by a new line
- *
  * Return: Always 0 (Success)
- * /
+ */
 int main(void)
 {
     _putchar('_');
@@ -18,5 +16,7 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
+
     return (0);
 }
+
