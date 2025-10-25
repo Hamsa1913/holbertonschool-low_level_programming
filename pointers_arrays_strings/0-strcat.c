21d4ece28eeacee845e7abf76b0fc330af086a09
Hams1913 <Hamsa1913@gmail.com>
2025-10-25 18:56:26 +0000

    update

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	/* نلقى نهاية النص الأول */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* ننسخ النص الثاني بعده */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
