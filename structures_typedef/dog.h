0df4460326c46c8af94f161c0f340522ed60cf53
Hamsa1913 <Hamsa12021@gmail.com>
2025-11-07 17:08:06 +0000

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,12 +6,14 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
+ * Description: Simple struct for practicing structures in C.
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-#endif
+#endif /* DOG_H */
