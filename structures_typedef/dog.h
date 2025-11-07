89e9738d81b2ee5be2c0869949ad117897002efa
Hamsa1913 <Hamsa12021@gmail.com>
2025-11-07 16:49:16 +0000

    update

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog info
+ * @name: dog's name (string)
+ * @age: dog's age (float)
+ * @owner: dog's owner (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
