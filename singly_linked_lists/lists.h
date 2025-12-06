6da152ea6e947a840fc0eec043bbd67615679585
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-06 10:47:34 +0000

    Add task 1: list_len

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,7 +11,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
@@ -21,6 +20,7 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
 
