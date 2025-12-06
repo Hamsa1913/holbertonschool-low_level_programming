06e1f033ea0d8e7874a536ed4e845c4bed225534
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-06 10:41:52 +0000

    Fix Betty style for singly linked lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,15 +11,16 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 
-#endif
+#endif /* LISTS_H */
 
