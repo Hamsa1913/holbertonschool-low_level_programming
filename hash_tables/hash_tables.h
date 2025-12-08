44175c34e63aacdf30b751d5f3313a117712e0f4
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-08 12:05:56 +0000

    Add hash_table_delete to free all allocated memory

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -24,31 +24,5 @@ typedef struct hash_node_s
  * @array: An array of size @size
  *
  * Each cell of this array is a pointer to the first node of a linked list
- * used for handling collisions (chaining).
- */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-/* Task 0 - Create hash table */
-hash_table_t *hash_table_create(unsigned long int size);
-
-/* Task 1 - djb2 algorithm */
-unsigned long int hash_djb2(const unsigned char *str);
-
-/* Task 2 - Key to index */
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
-/* Task 3 - Add or update key/value */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
-/* Task 4 - Retrieve value by key */
-char *hash_table_get(const hash_table_t *ht, const char *key);
-
-/* Task 5 - Print hash table */
-void hash_table_print(const hash_table_t *ht);
-
-#endif /* HASH_TABLES_H */
+ * used for handling colli*
 
