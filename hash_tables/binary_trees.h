6db273c8bc23843e4798cfc98e7548e3918d49fb
Hamsa1913 <Hamsa12021@gmail.com>
2025-12-08 11:36:28 +0000

    Add pre-order traversal function for binary trees

---
+++ hash_tables/binary_trees.h
@@ -0,0 +1,39 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/* Task 0 */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 6 - Pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Provided print function (not required to modify) */
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* BINARY_TREES_H */
+
